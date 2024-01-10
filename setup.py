3cac35c20fcdb18bd65da588c7fb13eeda9da6b5
Maxim Litvak <maxim.litvak@gmail.com>
2024-01-10 15:49:53 +0100

    update gitlab pipeline

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ long_description = """powerindex calculates some common power indices (related t
 setup(name='powerindex',
     version=os.environ['CI_COMMIT_TAG'],
     description='python library to calculate power indices in weighted voting games',
+    long_description=long_description,  # Include the long description
+    long_description_content_type='text/plain',  # Specify the content type, can be 'text/markdown' for Markdown
     author='Maxim Litvak',
     author_email='maxim@litvak.eu',
     url='http://gitlab.com/maxlit/powerindex',
@@ -35,4 +37,3 @@ setup(name='powerindex',
         ]
         }
                                          )
-
